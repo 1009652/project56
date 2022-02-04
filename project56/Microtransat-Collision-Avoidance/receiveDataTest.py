eaf158aa38eabaf22014f7f49eb657cc3d717e31
0966307 <0966307@hr.nl>
2022-02-04 11:07:15 +0100

    updated receievedata

--- project56/Microtransat-Collision-Avoidance/receiveDataTest.py
+++ project56/Microtransat-Collision-Avoidance/receiveDataTest.py
@@ -1,4 +1,8 @@
 import socket
+import serial
+from time import sleep
+
+port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=1)
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 10110
@@ -6,13 +10,8 @@ UDP_PORT = 10110
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    # print("received message: %s" % data)
-    # data = data[2:]
-    data = data.strip(b'\n')
-    # data = data.strip(b'\r')
-    # data = data[:1]
-    # print(data)
-    print(data.decode(encoding="ascii"))
+    print(data)
+    port.write(data)
+    sleep(0.001)
